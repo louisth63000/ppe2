3328145ee98d474dec59d390329dbc798173eec0
Cédric <astier50cedric@gmail.com>
2020-03-27 11:55:38 +0100

    bdd

---
+++ bbd.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Utilisateur 
+(idUtilisateur INT(11), 
+nom VARCHAR(64), 
+prenom VARCHAR(64), 
+mdp VARCHAR(64));
+
+CREATE TABLE Tacos
+(idTacos INT(11), 
+taille VARCHAR(64), 
+nomTacos VARCHAR(64)); 
+
+CREATE TABLE Sauce 
+( idSauce INT(11), 
+nomSauce VARCHAR(64));
+
+CREATE TABLE Viande
+( idViande INT(11), 
+nomViande VARCHAR(64));
+
+CREATE TABLE Boisson 
+(idBoisson INT(11), 
+nomBoisson VARCHAR(64)); 
+
+CREATE TABLE Commande 
+(idCommande INT(11));
+
+CREATE TABLE Panier 
+(idPanier INT(11));
+
+ALTER TABLE Utilisateur_
+
