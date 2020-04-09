f2f7ca6cf43936a64b4bfc07215192e8c3d043e7
Cédric <astier50cedric@gmail.com>
2020-04-09 11:55:52 +0200

    page accueil php

---
+++ ppe2_php/Cedric/pageaccueil.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php
+
+?>
+<link rel="stylesheet" type="text/css" href="pageaccueil.css" media="all"> 
+    <div class="container-blocks">
+        <div class="banniere">
+            <img class="image_banniere" src="banniere2.jpg" alt=""/>
+        </div>
+    </div>
+    <div class="container-blocks">
+        <div class="Commander"></div>
+        <div class="image">
+            <div class ="container-images">
+                <div class ="image1">
+                    <img class="image1" src="banniere.jpg" alt=""/>
+                </div>
+            </div>
+        </div>               
+    </div>
