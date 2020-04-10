47cae1fa680ec083047ddc11ef7e7eac62ea09dd
Cédric <astier50cedric@gmail.com>
2020-04-10 12:01:16 +0200

    save 10/04

--- ppe2_php/Cedric/pageaccueil.php
+++ ppe2_php/Cedric/pageaccueil.php
@@ -9,11 +9,24 @@
         </div>
     </div>
     <div class="container-blocks">
-        <div class="Commander"></div>
+        <div class="Commander">
+            <div class="bouton">
+                <p>
+                 <a href="#">Commander</a>
+               </p>
+            </div>
+        </div>
         <div class="image">
             <div class ="container-images">
-                <div class ="image1">
-                    <img class="image1" src="banniere.jpg" alt=""/>
+                <div class ="block-image1">
+                    <img class="groupe_image" src="banniere.jpg" alt=""/>
+                    <img class="groupe_image" src="image2.jpg" alt=""/>
+                </div>
+                <div class="container-images">
+                    <div class="block-image2">
+                        <img class="groupe_image" src="image3.jpg" alt=""/>                   
+                        <img class="groupe_image" src="image4.jpg" alt=""/>
+                    </div>
                 </div>
             </div>
         </div>               
