2228bf0c0a48e7ddfd23f6aa4c69a187a8064309
Cédric <astier50cedric@gmail.com>
2020-05-13 15:23:10 +0200

    Auto stash before merge of "master" and "origin/master"

--- ppe2_php/Cedric/pageaccueil.php
+++ ppe2_php/Cedric/pageaccueil.php
@@ -2,32 +2,19 @@
 <?php
 
 ?>
-<link rel="stylesheet" type="text/css" href="pageaccueil.css" media="all"> 
-    <div class="container-blocks">
-        <div class="banniere">
-            <img class="image_banniere" src="banniere2.jpg" alt=""/>
-        </div>
+<link rel="stylesheet" type="text/css" href="pageaccueil.css" media="all">
+<div>    
+    <div class="phrase">
+        <h2>Bienvenue chez Empire des Tacos</h2>
     </div>
-    <div class="container-blocks">
-        <div class="Commander">
-            <div class="bouton">
-                <p>
-                 <a href="#">Commander</a>
-               </p>
-            </div>
-        </div>
-        <div class="image">
-            <div class ="container-images">
-                <div class ="block-image1">
-                    <img class="groupe_image" src="banniere.jpg" alt=""/>
-                    <img class="groupe_image" src="image2.jpg" alt=""/>
-                </div>
-                <div class="container-images">
-                    <div class="block-image2">
-                        <img class="groupe_image" src="image3.jpg" alt=""/>                   
-                        <img class="groupe_image" src="image4.jpg" alt=""/>
-                    </div>
-                </div>
-            </div>
-        </div>               
+    <div class="bouton">
+            <p>
+                <a href="pagecommander.php" style="
+                    color: white;
+                    text-decoration: none;
+                ">Commander
+                </a>
+           </p>
     </div>
+</div> 
+
---
+++ ppe2_php/Cedric/pagecommander.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<?php
+?>
+<link rel="stylesheet" type="text/css" href="pagecommander.css" media="all">
+    <div class="phrase">
+        <h1> Quel menu souhaitez-vous commander ? </h1>
+    </div>
+    <div class="menu normal">
+        <h1> Menu normal </h1>       
+    </div>
+    <div class="menu xxl">
+        <h1> Menu XXL </h1>       
+    </div>
+
+
