94261a56b9588ca5842f7268344f8129804865e3
Cédric <astier50cedric@gmail.com>
2020-05-13 17:00:58 +0200

    page commander

--- ppe2_php/Cedric/pagecommander.php
+++ ppe2_php/Cedric/pagecommander.php
@@ -5,11 +5,20 @@
     <div class="phrase">
         <h1> Quel menu souhaitez-vous commander ? </h1>
     </div>
-    <div class="menu normal">
-        <h1> Menu normal </h1>       
+    <div class="container-blocks">
+        <div class="menu">
+            <div class="menu_m">
+                <h1> Menu M </h1>  
+            </div>
+            <div class="menu_l">
+                <h1> Menu L </h1>  
+            </div>
+            <div class="menu_xxl">
+                <h1> Menu XXL </h1>       
+            </div>
+        </div>
     </div>
-    <div class="menu xxl">
-        <h1> Menu XXL </h1>       
-    </div>
+
+
 
 
